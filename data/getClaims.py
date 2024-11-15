d233e10099255431650c8336b538a6285d1e4c87
skinreich11 <stavkin11@gmail.com>
2024-11-15 15:38:59 -0500

    Adding claims dataset

---
+++ data/getClaims.py
@@ -0,0 +1,21 @@
+import csv
+
+def process_csv(inputFile, outputFile):
+    claimsSet = set()
+    with open(inputFile, mode='r', encoding='utf-8') as infile:
+        reader = csv.DictReader(infile)
+        for row in reader:
+            claim = row['claim']
+            topicIP = int(row['topic_ip'])
+            if topicIP > 50 or '"' in claim:
+                continue
+            claimsSet.add((claim, topicIP))
+    with open(outputFile, mode='w', encoding='utf-8', newline='') as outFile:
+        writer = csv.writer(outFile)
+        writer.writerow(['claim', 'topic_ip'])
+        for claim, topicIP in claimsSet:
+            writer.writerow([claim, topicIP])
+inputFile =  '/path/to/HealthVer_dev.csv' # Replace with path to healthver_dev.csv file from the HealthVer dataset
+outputFile = './data/claims.csv'
+process_csv(inputFile, outputFile)
+print(f"Filtered CSV file has been saved to: {outputFile}")
